b4f8f33683d0c3b12ec0bc9eba8fbe5e6ededa73
DeepaliGhadge <108117371+DeepaliGhadge@users.noreply.github.com>
2022-11-23 15:30:50 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -18,9 +18,9 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id="8659e423-a7ae-4e8f-9d43-3f63027b6e19"
-  tenant_id="b3729ee8-cde4-407d-9f7b-7316a3be9359"
-  client_id ="8bd181e4-43f8-465c-b294-410c3508622e"
-  client_secret="H1K8Q~y.90l6tUMbSDEGvvtgPxVb5y9hoAhRcaXY"
+  subscription_id="{subscription_id}"
+  tenant_id="{tenant_id}"
+  client_id ="{client_id}"
+  client_secret="{client_secret}"
   features {}
  }
